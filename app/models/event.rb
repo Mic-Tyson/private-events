67423179c12281c34e3a1c66a320a8e8922347f8
Mic-Tyson <mkooner@live.com>
2024-09-21 23:18:13 +0100

    add scopes for timed events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,11 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :events_users
   has_many :attendees, through: :events_users, source: :user
+
+  scope :upcoming, ->(date = Date.today) { where('date >= ?', date) }
+  scope :expired, ->(date = Date.today) { where('date < ?', date) }
+
+  def expired?
+    date < Date.today
+  end
 end
