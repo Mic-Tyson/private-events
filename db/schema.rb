9e9c4d703c575be6658ec3a49090f44b4a4f9f5a
Mic-Tyson <mkooner@live.com>
2024-09-21 15:37:37 +0100

    rollback db changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_21_141239) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_21_124051) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.datetime "date"
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_21_141239) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
