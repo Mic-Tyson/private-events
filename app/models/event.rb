24ab9427d1940810d80ad9a21aa1f9c578744db6
Mic-Tyson <mkooner@live.com>
2024-09-21 14:37:39 +0100

    move join table declaration

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  has_many :attendees, through: :events_users, source: :user
-
   has_many :events_users
+  has_many :attendees, through: :events_users, source: :user
 end
